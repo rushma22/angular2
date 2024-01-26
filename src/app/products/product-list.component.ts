a38612dc49bcdd7b394503e3bc14314842128799
rushma22 <rushma.najeeb@gmail.com>
2024-01-26 23:06:38 +0530

    day 3

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,28 +2,45 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-product',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+ 
 })
 
 export class ProductListComponent {
-    pageHeader: string ="Product List";
+    pageHeader: string = "Product List";
+    imageMargin: number = 2;
+    imageWidth: number = 50;
+    showImage: boolean = false;
+    listFilter: string = " ";
+    buttonPadding: number = 10;
+    colour: string = "orange";
+
     products: any[] = [
         {
             "productName": "Hammer",
-            "quantity": 20
+            "quantity": 20,
+            "imgUrl": "assets/hammer.jpg"
+            
         },
         {
             "productName": "Spanner",
-            "quantity": 32
+            "quantity": 32,
+            "imgUrl": "assets/spanner.jpg"
         },
         {
             "productName": "Nails",
-            "quantity": 45
+            "quantity": 45,
+            "imgUrl": "assets/nails.jpg"
         },
         {
-            "productName": "Grilling machine",
-            "quantity": 10
+            "productName": "Saw",
+            "quantity": 10,
+            "imgUrl": "assets/saw.jpeg"
         }
        
     ];
+
+    toggleImage(): void{
+        this.showImage = !this.showImage;
+    }
 }
